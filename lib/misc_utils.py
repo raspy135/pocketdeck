770de9f269da2bc38573103dd9e0161fc5c874e4
ryosuke kojima <ryosukekojima@ryosukes-MacBook-Air.local>
2026-03-09 22:20:41 +0900

    adding misc_utils.py

---
+++ lib/misc_utils.py
@@ -0,0 +1,26 @@
+import esclib as ec
+import pdeck_utils
+
+def input(message):
+  vs = pdeck_utils.vscreen_stream()
+  return input_vs(vs, message)
+  
+def input_vs(vs, message):
+  elib = ec.esclib()
+  print(message, file=vs)
+  out=''
+  while True:
+    key = vs.read(1).encode('utf-8')
+    #print(key, file=vs)
+    str = key.decode('utf-8')
+    if key == b'\x08':
+      out = out[:-1]
+      vs.v.print(elib.cur_left(1))
+      vs.v.print(elib.erase_to_end_of_current_line())
+      continue
+    vs.v.print(str)
+    out += str
+    if key == b'\x0d':
+      break
+  return out
+
